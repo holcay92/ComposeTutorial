1e34d393e720b68b5c3ddebf74bf5aedf7b0d887
H.Halil Olcay <holcay@marun.edu.tr>
2023-09-18 22:38:02 +0300

    snackbar example with toast

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,90 +1,78 @@
 package com.example.composetutorial
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SnackBarExample()
+            SnackbarDemo()
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SnackBarExample() {
-    var snackBarVisible by remember { mutableStateOf(false) }
-
+fun SnackbarDemo() {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "snackBar Example") },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text("Show snackbar") },
+                icon = { Icon(Icons.Filled.Call, contentDescription = "") },
+                onClick = {
+                    scope.launch {
+                        val result = snackbarHostState
+                            .showSnackbar(
+                                message = "Snackbar",
+                                actionLabel = "Action",
+                                // Defaults to SnackbarDuration.Short
+                                duration = SnackbarDuration.Short,
+                            )
+                        when (result) {
+                            SnackbarResult.ActionPerformed -> {
+                                Toast.makeText(context, "Action performed", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+
+                            SnackbarResult.Dismissed -> {
+                                Toast.makeText(context, "Action dismissed", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    }
+                },
             )
         },
-        content = {
-            Column(
-                modifier = Modifier.padding(paddingValues = it)
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Button(
-                    onClick = {
-                        snackBarVisible = true
-                    },
-                    modifier = Modifier.padding(8.dp),
-                ) {
-                    Text(text = "Show snackBar")
-                }
-
-                if (snackBarVisible) {
-                    Snackbar(
-                        modifier = Modifier.padding(8.dp),
-
-                        action = {
-                            TextButton(
-                                onClick = {
-                                    snackBarVisible = false
-                                },
-                            ) {
-                                Text(text = "Dismiss")
-                            }
-                        },
-                    ) {
-                        Text(text = "example message.")
-                    }
-                }
-            }
-        },
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun Preview() {
-    SnackBarExample()
+    ) { contentPadding ->
+        Text(text = "hello world", modifier = Modifier.padding(contentPadding))
+    }
 }
