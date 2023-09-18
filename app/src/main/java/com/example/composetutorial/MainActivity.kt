823fff6561f08f68b273ee7283b8b4d3a8665dd8
H.Halil Olcay <holcay@marun.edu.tr>
2023-09-18 19:04:38 +0300

    box example

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,54 +3,61 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-            ) {
-                Greeting(name = "Halil")
-                Greeting(name = "Halil")
-                Greeting(name = "Halil")
-            }
+            BoxExample()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(
-        text = "Hello $name!",
-        modifier = Modifier.background(Color.Yellow)
-            .border(3.dp, Color.Green).padding(16.dp),
-        fontSize = 32.sp,
-        fontWeight = FontWeight.Bold,
-        color = Color.Red,
-    )
-}
+fun BoxExample() {
+    Box(
+        modifier = Modifier
+            .background(Color.LightGray),
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.image_face),
+            contentDescription = "dice image",
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Greeting("Halil olcay!")
+        )
+        Text(
+            text = "test img",
+            modifier = Modifier
+                .background(Color.White)
+                .align(Alignment.BottomStart),
+        )
+        Button(
+            onClick = { },
+            colors = ButtonDefaults.textButtonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Black,
+            ),
+            modifier = Modifier
+
+                .align(Alignment.TopEnd)
+                .border(5.dp, Color.Red, RectangleShape),
+        ) {
+            Text(text = "Roll The Dice")
+
+        }
+    }
 }
