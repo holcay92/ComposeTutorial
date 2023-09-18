536fbbff4deb4b590c34527bdfeb6da22ea8c053
H.Halil Olcay <holcay@marun.edu.tr>
2023-09-18 21:56:16 +0300

    button types exampples

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,63 +1,91 @@
 package com.example.composetutorial
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BoxExample()
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                ButtonDemo()
+            }
         }
     }
 }
 
 @Composable
-fun BoxExample() {
-    Box(
-        modifier = Modifier
-            .background(Color.LightGray),
+fun ButtonDemo() {
+    val context = LocalContext.current
+    Button(
+        onClick = { Toast.makeText(context, "Button Clicked", Toast.LENGTH_SHORT).show() },
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.image_face),
-            contentDescription = "dice image",
-
+        Text(text = "Button")
+    }
+    Button(
+        onClick = { Toast.makeText(context, "Button Clicked 2", Toast.LENGTH_SHORT).show() },
+        enabled = false,
+    ) {
+        Text(text = "Button")
+    }
+    TextButton(
+        onClick = { Toast.makeText(context, "TextButton Clicked", Toast.LENGTH_SHORT).show() },
+    ) {
+        Text(text = "Button")
+    }
+    OutlinedButton(
+        onClick = { Toast.makeText(context, "OutlinedButton Clicked", Toast.LENGTH_SHORT).show() },
+    ) {
+        Text(text = "Button")
+    }
+    IconButton(onClick = {
+        Toast.makeText(context, "IconButton Clicked", Toast.LENGTH_SHORT).show()
+    }) {
+        Icon(
+            Icons.Filled.Refresh,
+            contentDescription = "Refresh Icon",
+            tint = Color.Red,
+            modifier = Modifier.size(60.dp),
         )
-        Text(
-            text = "test img",
-            modifier = Modifier
-                .background(Color.White)
-                .align(Alignment.BottomStart),
-        )
-        Button(
-            onClick = { },
-            colors = ButtonDefaults.textButtonColors(
-                containerColor = Color.Transparent,
-                contentColor = Color.Black,
-            ),
-            modifier = Modifier
-
-                .align(Alignment.TopEnd)
-                .border(5.dp, Color.Red, RectangleShape),
-        ) {
-            Text(text = "Roll The Dice")
-
-        }
+    }
+    Button(
+        onClick = { Toast.makeText(context, "Styled Button Clicked", Toast.LENGTH_SHORT).show() },
+        shape = CutCornerShape(10.dp),
+        contentPadding = PaddingValues(16.dp),
+        border = BorderStroke(10.dp, Color.Gray),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Green,
+            contentColor = Color.White,
+        ),
+    ) {
+        Text(text = "Button")
     }
 }
